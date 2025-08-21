8ec45330098b33521b738e0f8a98749fdc48bf75
matheus.sant <matheusantana731@gmail.com>
2025-08-21 16:49:24 -0300

    Adicionando a o exercicio da tabuada

---
+++ aula06/script.js
@@ -0,0 +1,22 @@
+const bt = document.querySelector("button#bt");
+const limpar = document.querySelector("button#clear")
+const res = document.querySelector("div#res");
+let txtn1 = document.querySelector("input#txtn1");
+
+bt.addEventListener('click', function() {
+    let n = parseInt(txtn1.value)
+    res.innerHTML = "";
+
+    if (isNaN(n)) {
+        res.innerHTML = `digite algo no campo number`;
+        return
+    }
+
+    for (let i = 1; i <= 10; i++) {
+        res.innerHTML += `${n} x ${i} = ${n*i}<br>`
+    }
+});
+
+limpar.addEventListener('click', function() {
+    res.innerHTML = ""
+})
